3657fce3fb39ef7dd183ff096997dae62b9c9b9a
sai <kskprasad96@gmail.com>
2021-02-23 22:25:02 +0530

    react router setup

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,17 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Router>
+        <Switch>
+          {/* TODO: ADD routes */}
+          {/* TODO: Handle Non Existant Routes - redirect to /404 */}
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
