6c4775234f950e970d16121e26775d72e5cbf26e
sai <kskprasad96@gmail.com>
2021-02-24 10:17:42 +0530

    add filter functionality for name and company filters

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { useFetch } from '../../hooks/useFetch';
 
 interface User {
@@ -18,11 +18,36 @@ function Home() {
   const [nameFilter, setNameFilter] = useState('');
   const [companyFilter, setCompanyFilter] = useState('');
   const { response: users, error, loading } = useFetch<User>('/users');
+  const initialUsers = useMemo(() => users ?? [], [users]);
+  const [filteredUsers, setFilteredUsers] = useState(initialUsers);
 
   const clearFilters = () => {
     setNameFilter('');
     setCompanyFilter('');
+    setFilteredUsers(initialUsers);
   };
+
+  useEffect(() => {
+    if (!nameFilter && !companyFilter) {
+      setFilteredUsers(initialUsers);
+      return;
+    }
+
+    let filteredUserList = initialUsers;
+    if (!!nameFilter) {
+      filteredUserList = filteredUserList.filter(({ name }) =>
+        name.toLowerCase().includes(nameFilter.toLowerCase())
+      );
+    }
+
+    if (!!companyFilter) {
+      filteredUserList = filteredUserList.filter(({ company: { name } }) =>
+        name.toLowerCase().includes(companyFilter.toLowerCase())
+      );
+    }
+
+    setFilteredUsers(filteredUserList);
+  }, [nameFilter, companyFilter, initialUsers]);
 
   if (error) {
     return <h2>{error}</h2>;
@@ -48,28 +73,31 @@ function Home() {
         )}
       </div>
       {loading && <h2>Fetching Users... Sit Tight!</h2>}
-      {users !== null && (
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Company</th>
-              <th></th>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Company</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredUsers.map(({ id, name, username, company }) => (
+            <tr key={`${username}-${id}`}>
+              <td>{name}</td>
+              <td>{company.name}</td>
+              <td>
+                <a href={`/posts?userId=${id}`}>View Posts</a>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {users.map(({ id, name, username, company }) => (
-              <tr key={`${username}-${id}`}>
-                <td>{name}</td>
-                <td>{company.name}</td>
-                <td>
-                  <a href={`/posts?userId=${id}`}>View Posts</a>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
+          ))}
+          {!filteredUsers.length && (
+            <tr>
+              <td colSpan={3}>No users matching search term....</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </div>
   );
 }
