fa7de58f64c6cb3b60fb1c3f075d3167829980bb
sai <kskprasad96@gmail.com>
2021-02-25 16:42:17 +0530

    pagination styles

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { DEFAULT_COUNT_PER_PAGE } from '../../constants';
+import './paginationStyles.scss';
 
 interface PaginationData {
   page: number;
@@ -20,39 +21,56 @@ function Pagination({
   const start = (page - 1) * limit + 1;
   const end = Math.min(start - 1 + limit, total);
   return (
-    <div>
-      <label>
-        Posts Per Page:
-        <input
-          type="number"
-          min="1"
-          step="1"
-          max={total || 1}
-          value={limit || DEFAULT_COUNT_PER_PAGE}
-          onChange={({ target }) =>
-            handlePostsPerPageChange(
-              Number(target.value ?? DEFAULT_COUNT_PER_PAGE)
-            )
-          }
-          disabled={limit < 1}
-        />
-      </label>
-      <span>
-        Showing&nbsp;{start} - {end}
-      </span>{' '}
-      of{' '}
-      <span>
-        <b>{total}</b>
-      </span>
-      <button onClick={() => handleChangePage(page - 1)} disabled={start <= 1}>
-        prev
-      </button>
-      <button
-        onClick={() => handleChangePage(page + 1)}
-        disabled={end >= total}
-      >
-        next
-      </button>
+    <div className="pagination-container">
+      <div className="per-page-limit">
+        <label htmlFor="perPageLimit" className="per-page-limit__label">
+          <input
+            id="perPageLimit"
+            className="per-page-limit__input"
+            type="number"
+            min="1"
+            step="1"
+            max={total || 1}
+            value={limit || DEFAULT_COUNT_PER_PAGE}
+            onChange={({ target }) =>
+              handlePostsPerPageChange(
+                Number(target.value ?? DEFAULT_COUNT_PER_PAGE)
+              )
+            }
+            disabled={limit < 1}
+          />
+          <span>{'/ Page'}</span>
+        </label>
+      </div>
+      <div className="page-meta-data">
+        <div className="page-meta-data__info">
+          <span className="page-meta-data__bounds">
+            <b>
+              {start}&nbsp;&minus;&nbsp;{end}
+            </b>
+          </span>
+          &nbsp;of&nbsp;
+          <span className="page-meta-data__total">
+            <b>{total}</b>
+          </span>
+        </div>
+        <div className="page-meta-data__navigators">
+          <button
+            onClick={() => handleChangePage(page - 1)}
+            disabled={start <= 1}
+            className="prev"
+          >
+            prev
+          </button>
+          <button
+            onClick={() => handleChangePage(page + 1)}
+            disabled={end >= total}
+            className="next"
+          >
+            next
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
