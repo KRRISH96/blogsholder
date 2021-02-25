ae4ca47018c8ef90b285bbad8c7042b08339248a
sai <kskprasad96@gmail.com>
2021-02-25 11:33:05 +0530

    invalid url redirection

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,10 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 import Home from './Home/Home';
 import Posts from './Posts/Posts';
 import PostDetails from './PostDetails/PostDetails';
@@ -12,7 +17,10 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/posts" component={Posts} />
           <Route path="/posts/:id" component={PostDetails} />
-          {/* TODO: Handle Non Existant Routes - redirect to /404 */}
+          {/* Redirect to Home Page on non-exitant routes. This can also be redirected to a /404 page */}
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
         </Switch>
       </Router>
     </div>
